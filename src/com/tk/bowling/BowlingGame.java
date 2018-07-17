b7d55dbc6d2271770ad3e941a23fce8c3cfe98f2
Tomasz Koziara <tomaszkoziara86@gmail.com>
2018-07-17 23:15:01 +0200

    Refactor

--- src/com/tk/bowling/BowlingGame.java
+++ src/com/tk/bowling/BowlingGame.java
@@ -14,12 +14,12 @@ public class BowlingGame {
         int score = 0;
         int roll = 0;
         for (int frame = 0; frame < 10; frame++) {
-            if (rolls[roll] == 10) {
+            if (isStrike(roll)) {
                 score += rolls[roll];
                 score += rolls[roll + 1];
                 score += rolls[roll + 2];
                 roll++;
-            } else if (rolls[roll] + rolls[roll + 1] == 10) {
+            } else if (isSpare(roll)) {
                 score += rolls[roll] + rolls[roll + 1] + rolls[roll + 2];
                 roll += 2;
             } else {
@@ -30,5 +30,14 @@ public class BowlingGame {
         }
         return score;
     }
+
+    private boolean isSpare(int roll) {
+        return rolls[roll] + rolls[roll + 1] == 10;
+    }
+
+    private boolean isStrike(int roll) {
+        return rolls[roll] == 10;
+    }
+
 
 }
