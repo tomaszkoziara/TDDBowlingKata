6b7f107a0dae5c470c9c80bb8538157092a8d7c1
Tomasz Koziara <tomaszkoziara86@gmail.com>
2018-07-17 22:48:29 +0200

    Refactor

--- src/com/tk/bowling/test/BowlingGameTest.java
+++ src/com/tk/bowling/test/BowlingGameTest.java
@@ -1,15 +1,22 @@
 package com.tk.bowling.test;
 
 import com.tk.bowling.BowlingGame;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BowlingGameTest {
 
+    BowlingGame bowlingGame;
+
+    @BeforeEach
+    public void setUp() {
+        bowlingGame = new BowlingGame();
+    }
+
     @Test
     public void testAllMisses() {
-        BowlingGame bowlingGame = new BowlingGame();
         for (int i = 0; i < 20; i++) {
             bowlingGame.roll(0);
         }
@@ -18,7 +25,6 @@ public class BowlingGameTest {
 
     @Test
     public void testAllOnePin() {
-        BowlingGame bowlingGame = new BowlingGame();
         for (int i = 0; i < 20; i++) {
             bowlingGame.roll(1);
         }
