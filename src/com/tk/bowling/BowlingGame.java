e351e190953b256e0af23baaa0cf2306c271d04a
Tomasz Koziara <tomaszkoziara86@gmail.com>
2018-07-17 22:56:38 +0200

    Refactor

--- src/com/tk/bowling/BowlingGame.java
+++ src/com/tk/bowling/BowlingGame.java
@@ -2,13 +2,19 @@ package com.tk.bowling;
 
 public class BowlingGame {
 
-    private int score = 0;
+    private int[] rolls = new int[23];
+    private int currentRoll = 0;
 
     public void roll(int pins) {
-        score += pins;
+        rolls[currentRoll] = pins;
+        currentRoll++;
     }
 
     public int score() {
+        int score = 0;
+        for (int i = 0; i < rolls.length; i++) {
+            score += rolls[i];
+        }
         return score;
     }
 
